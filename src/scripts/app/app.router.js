cf4c6ddb2156fc52cca889118a50ee6457932b79
pcboby <pcboby@126.com>
2017-04-06 23:54:53 +0800

    re

--- src/scripts/app/app.router.js
+++ src/scripts/app/app.router.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Dashboard from '@/views/Dashboard/index.vue'
 // Dashboard
-import Bridge from '@/views/Bridge/index.vue'
+import Dashboard from '@/views/Dashboard/index.vue'
 // 桥页：宣传页
-import Group from '@/views/Group/index.vue'
+import Bridge from '@/views/Bridge/index.vue'
 // 群组
-import Project from '@/views/Project/index.vue'
+import Group from '@/views/Group/index.vue'
 // 项目
-import Signup from '@/views/Signup/index.vue'
+import Project from '@/views/Project/index.vue'
 // 报名注册
+import Signup from '@/views/Signup/index.vue'
 
 Vue.use(Router)
 
--- src/scripts/app/app.ui.js
+++ src/scripts/app/app.ui.js
@@ -1,3 +1,4 @@
+// 加载基础样式库
 // import 'animate.css/animate.css'
 import 'muse-ui/dist/muse-ui.css'
 import 'muse-ui/dist/theme-carbon.css'
@@ -6,7 +7,7 @@ import 'simple-line-icons/css/simple-line-icons.css'
 import 'font-awesome/css/font-awesome.css'
 // 加载自定义样式库
 import '@/themes/default/css/style.css'
-
+// 加载muse-ui插件库
 import Vue from 'vue'
 import MUI from 'muse-ui'
 
